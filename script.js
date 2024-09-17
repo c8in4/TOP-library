9ae207ec9bb12d8faacc32b55c2bfb84cd3845d8
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-17 20:58:20 +1200

    Add form validation
    Use javascript constraint API to validate input.
    Title, author and pages are required inputs.

--- script.js
+++ script.js
@@ -1,141 +1,146 @@
-const dialog = document.querySelector("dialog");
-const openDialogButton = document.querySelector("#open-dialog");
-const addBookButton = document.querySelector("#add-book-button");
-const closeDialogButton = document.querySelector("#close-dialog-button");
-const display = document.querySelector("#display");
+const dialog = document.querySelector("dialog")
+const openDialogButton = document.querySelector("#open-dialog")
+const addBookButton = document.querySelector("#add-book-button")
+const closeDialogButton = document.querySelector("#close-dialog-button")
+const display = document.querySelector("#display")
 
 class Book {
   constructor(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
   }
 
   changeStatus = () => {
-    this.status = !this.status;
+    this.status = !this.status
   }
-};
+}
 
 const myLibrary = [
   new Book("Pride and Prejudice", "	Jane Austen", 259, true),
   new Book("The Great Gatsby", "Scott Fitzgerald", 180, false),
   new Book("To Kill a Mockingbird", "Harper Lee", 281, false),
-];
+]
 
 openDialogButton.addEventListener("click", () => {
-  dialog.showModal();
-});
+  dialog.showModal()
+})
 
 closeDialogButton.addEventListener("click", () => {
-  dialog.close();
-});
+  dialog.close()
+})
+
+dialog.addEventListener("close", () => {
+  form.reset()
+  console.log("dialog closed")
+})
+
+const form = document.querySelector("form")
+const title = document.querySelector("#title")
+const author = document.querySelector("#author")
+const pages = document.querySelector("#pages")
+const status = document.querySelector("#status")
 
 addBookButton.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  const form = document.querySelector("form");
-  const title = document.querySelector("#title");
-  const author = document.querySelector("#author");
-  const pages = document.querySelector("#pages");
-  const status = document.querySelector("#status");
-
-  addBook(title.value, author.value, pages.value, status.checked);
-  loadMyLibrary();
+  e.preventDefault()
+  if (
+    title.checkValidity() &&
+    author.checkValidity() &&
+    pages.checkValidity()
+  ) {
+    addBook(title.value, author.value, pages.value, status.checked)
+    loadMyLibrary()
+  }
 
   form.reset()
-  dialog.close();
-});
+  dialog.close()
+})
 
 function addBook(title, author, pages, status) {
-  author = !author ? "unknown" : author;
-  pages = !pages ? "unknown" : pages;
-
-  if (title) {
-    myLibrary.push(new Book(title, author, pages, status));
-    sortMyLibrary();
-  }
-};
+  myLibrary.push(new Book(title, author, pages, status))
+}
 
 function deleteBook(title, index) {
   confirm(`Are you sure you want to delete "${title}"?`)
     ? delete myLibrary[index]
-    : false;
+    : false
 }
 
 function sortMyLibrary() {
   myLibrary.sort((a, b) => {
-    const titleA = a.title.toUpperCase();
-    const titleB = b.title.toUpperCase();
+    const titleA = a.title.toUpperCase()
+    const titleB = b.title.toUpperCase()
     if (titleA < titleB) {
-      return -1;
+      return -1
     }
     if (titleA > titleB) {
-      return 1;
+      return 1
     }
-    return 0;
-  });
+    return 0
+  })
 }
 
 function loadMyLibrary() {
-  display.textContent = "";
-  myLibrary.forEach(book => {
-    createCard(book);
-  });
+  display.textContent = ""
+  sortMyLibrary()
+  myLibrary.forEach((book) => {
+    createCard(book)
+  })
 }
 
 function createCard(book) {
-  const card = document.createElement("div");
-  const infos = document.createElement("div");
-  const title = document.createElement("h2");
-  const author = document.createElement("p");
-  const pages = document.createElement("p");
-  const status = document.createElement("p");
-  const buttons = document.createElement("div");
-  const statusButton = document.createElement("button");
-  const deleteButton = document.createElement("img");
+  const card = document.createElement("div")
+  const infos = document.createElement("div")
+  const title = document.createElement("h2")
+  const author = document.createElement("p")
+  const pages = document.createElement("p")
+  const status = document.createElement("p")
+  const buttons = document.createElement("div")
+  const statusButton = document.createElement("button")
+  const deleteButton = document.createElement("img")
 
   statusButton.addEventListener("click", () => {
-    book.changeStatus();
-    loadMyLibrary();
-  });
+    book.changeStatus()
+    loadMyLibrary()
+  })
 
   deleteButton.addEventListener("click", () => {
-    deleteBook(book.title, myLibrary.indexOf(book));
-    loadMyLibrary();
-  });
+    deleteBook(book.title, myLibrary.indexOf(book))
+    loadMyLibrary()
+  })
 
-  deleteButton.setAttribute("class", "delete-button");
-  deleteButton.setAttribute("src", "icons/delete.svg");
-  deleteButton.setAttribute("alt", "delete button");
+  deleteButton.setAttribute("class", "delete-button")
+  deleteButton.setAttribute("src", "icons/delete.svg")
+  deleteButton.setAttribute("alt", "delete button")
 
-  title.textContent = book.title;
-  author.textContent = "Author: " + book.author;
-  pages.textContent = "Pages: " + book.pages;
+  title.textContent = book.title
+  author.textContent = "Author: " + book.author
+  pages.textContent = "Pages: " + book.pages
 
-  let read = "not read yet";
-  let buttonText = '"read"';
+  let read = "not read yet"
+  let buttonText = '"read"'
   if (book.status) {
-    read = "already read";
-    buttonText = '"not read"';
-  };
-  status.textContent = "Status: " + read;
-  statusButton.textContent = "Mark as " + buttonText;
+    read = "already read"
+    buttonText = '"not read"'
+  }
+  status.textContent = "Status: " + read
+  statusButton.textContent = "Mark as " + buttonText
 
-  buttons.setAttribute("class", "card-buttons");
-  buttons.appendChild(statusButton);
-  buttons.appendChild(deleteButton);
+  buttons.setAttribute("class", "card-buttons")
+  buttons.appendChild(statusButton)
+  buttons.appendChild(deleteButton)
 
-  infos.appendChild(title);
-  infos.appendChild(author);
-  infos.appendChild(pages);
-  infos.appendChild(status);
+  infos.appendChild(title)
+  infos.appendChild(author)
+  infos.appendChild(pages)
+  infos.appendChild(status)
 
-  card.setAttribute("class", "card");
-  card.appendChild(infos);
-  card.appendChild(buttons);
+  card.setAttribute("class", "card")
+  card.appendChild(infos)
+  card.appendChild(buttons)
 
-  display.appendChild(card);
+  display.appendChild(card)
 }
 
-loadMyLibrary();
+loadMyLibrary()
